ceaa2430f52da0b6b9ffcedba2441321ff1037d4
henkka <henrik.lappi@outlook.com>
2021-04-14 09:30:20 +0300

    Made try/catch validations to notesResolver mutations.

--- src/graphql/notes/notesResolver.js
+++ src/graphql/notes/notesResolver.js
@@ -1,26 +1,5 @@
-const { v1: uuid } = require('uuid')
 const { UserInputError } = require('apollo-server')
 const Note = require('../../models/note.js')
-
-let notes = [
-    {
-        title: "Hae ruoka",
-        text: "osta omenoita ja kahvia",
-        id: "3d594650-3436-11e9-bc57-8b80ba54c431"
-    },
-    {
-        title: "Hyvä blogikirjoitus",
-        text: "Käsittelee ohjelmointia",
-        url: "www.jokuosoite.com",
-        important: true,
-        id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-    },
-    {
-        title: "Vie roskat",
-        important: false,
-        id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-    },
-]
 
 module.exports = {
     Query: {
@@ -31,24 +10,51 @@ module.exports = {
         findNote: (root, args) => Note.findOne({ title: args.title })
     },
     Mutation: {
-        addNote: (root, args) => {
+        addNote: async (root, args) => {
             const note = new Note({ ...args })
-            return note.save()
+
+            try {
+                await note.save()
+            } catch (error) {
+                throw new UserInputError(error.message, {
+                    invalidArgs: args,
+                })
+            }
+
+            return note
         },
         editNote: async (root, args) => {
             let important = args.important
-            const note = await Note.findOne({ _id: args.id })                
+            const note = await Note.findOne({ _id: args.id })
             if (args.important == null) {
                 important = note.important
-            }         
+            }
+
             note.title = args.title || note.title
             note.text = args.text || note.text
             note.url = args.url || note.url
             note.important = important
-            return note.save()
+
+            try {
+                await note.save()
+              } catch (error) {
+                throw new UserInputError(error.message, {
+                  invalidArgs: args,
+                })
+              }
+
+            return note
         },
-        deleteNote: (root, args) => {
-            return Note.deleteOne({ _id: args.id })
+        deleteNote: async (root, args) => {
+            try {
+                note = await Note.deleteOne({ _id: args.id })
+              } catch (error) {
+                throw new UserInputError(error.message, {
+                  invalidArgs: args,
+                })
+              }
+
+            return note
         }
     }
 }
